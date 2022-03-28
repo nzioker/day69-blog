5b665dda90467bd570224019cb34e6847577d30c
Ken <nzioker@gmail.com>
2022-03-28 11:50:14 +0300

    Configure the db uri and secret key to be accessed by the environment variables. make the sqlite db as secondary to allow as a secondary local development.

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ from forms import CreatePostForm, RegisterForm, LoginForm
 from flask_gravatar import Gravatar
 from flask import abort
 from functools import wraps
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL, sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
